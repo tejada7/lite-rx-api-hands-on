e332a1523b292d7341549f011df77a3a41abdc70
Favio Tejada <favio_tejada@hotmail.com>
2022-02-14 11:42:37 +0100

    Solution part 5 merge

--- src/main/java/io/pivotal/literx/Part05Merge.java
+++ src/main/java/io/pivotal/literx/Part05Merge.java
@@ -11,25 +11,20 @@ import reactor.core.publisher.Mono;
  */
 public class Part05Merge {
 
-//========================================================================================
+    //========================================================================================
+    Flux<User> mergeFluxWithInterleave(Flux<User> flux1, Flux<User> flux2) {
+        return flux1.mergeWith(flux2);
+    }
 
-	// TODO Merge flux1 and flux2 values with interleave
-	Flux<User> mergeFluxWithInterleave(Flux<User> flux1, Flux<User> flux2) {
-		return null;
-	}
+    //========================================================================================
+    Flux<User> mergeFluxWithNoInterleave(Flux<User> flux1, Flux<User> flux2) {
+        return flux1.concatWith(flux2);
+    }
 
-//========================================================================================
-
-	// TODO Merge flux1 and flux2 values with no interleave (flux1 values and then flux2 values)
-	Flux<User> mergeFluxWithNoInterleave(Flux<User> flux1, Flux<User> flux2) {
-		return null;
-	}
-
-//========================================================================================
-
-	// TODO Create a Flux containing the value of mono1 then the value of mono2
-	Flux<User> createFluxFromMultipleMono(Mono<User> mono1, Mono<User> mono2) {
-		return null;
-	}
+    //========================================================================================
+    Flux<User> createFluxFromMultipleMono(Mono<User> mono1, Mono<User> mono2) {
+//		return Flux.from(mono1).concatWith(mono2);
+        return Flux.concat(mono1, mono2);
+    }
 
 }
