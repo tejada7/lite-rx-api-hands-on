2a79b508a76ceb7cd8fdcf9d5fc11f0a9015b47d
Favio Tejada <favio_tejada@hotmail.com>
2022-02-14 10:16:30 +0100

    Solution part 1 flux

--- src/main/java/io/pivotal/literx/Part01Flux.java
+++ src/main/java/io/pivotal/literx/Part01Flux.java
@@ -2,6 +2,9 @@ package io.pivotal.literx;
 
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.util.Arrays;
+
 /**
  * Learn how to create Flux instances.
  *
@@ -10,39 +13,35 @@ import reactor.core.publisher.Flux;
  */
 public class Part01Flux {
 
-//========================================================================================
+    //========================================================================================
+    Flux<String> emptyFlux() {
+//		return Flux.just();
+        return Flux.empty();
+    }
 
-	// TODO Return an empty Flux
-	Flux<String> emptyFlux() {
-		return null;
-	}
+    //========================================================================================
+    Flux<String> fooBarFluxFromValues() {
+//		return Flux.fromStream(() -> Stream.of("foo", "bar"));
+        return Flux.just("foo", "bar");
+    }
 
-//========================================================================================
+    //========================================================================================
+    Flux<String> fooBarFluxFromList() {
+        return Flux.fromIterable(Arrays.asList("foo", "bar"));
+    }
 
-	// TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
-	Flux<String> fooBarFluxFromValues() {
-		return null;
-	}
+    //========================================================================================
+    Flux<String> errorFlux() {
+        return Flux.error(IllegalStateException::new);
+    }
 
-//========================================================================================
-
-	// TODO Create a Flux from a List that contains 2 values "foo" and "bar"
-	Flux<String> fooBarFluxFromList() {
-		return null;
-	}
-
-//========================================================================================
-
-	// TODO Create a Flux that emits an IllegalStateException
-	Flux<String> errorFlux() {
-		return null;
-	}
-
-//========================================================================================
-
-		// TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
-	Flux<Long> counter() {
-		return null;
-	}
+    //========================================================================================
+    Flux<Long> counter() {
+        return Flux.range(0, 10)
+                .map(Long::valueOf)
+                .delayElements(Duration.ofMillis(100));
+//        return Flux.interval(Duration.ofMillis(100))
+//                .take(10);
+    }
 
 }
--- src/test/java/io/pivotal/literx/Part01FluxTest.java
+++ src/test/java/io/pivotal/literx/Part01FluxTest.java
@@ -10,57 +10,47 @@ import reactor.test.StepVerifier;
  * @author Sebastien Deleuze
  * @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html">Flux Javadoc</a>
  */
-public class Part01FluxTest {
+class Part01FluxTest {
 
-	Part01Flux workshop = new Part01Flux();
+    Part01Flux workshop = new Part01Flux();
 
-//========================================================================================
+    //========================================================================================
+    @Test
+    void empty() {
+        Flux<String> flux = workshop.emptyFlux();
 
-	@Test
-	public void empty() {
-		Flux<String> flux = workshop.emptyFlux();
+        StepVerifier.create(flux)
+                .verifyComplete();
+    }
 
-		StepVerifier.create(flux)
-				.verifyComplete();
-	}
+    //========================================================================================
+    @Test
+    void fromValues() {
+        StepVerifier.create(workshop.fooBarFluxFromValues())
+                .expectNext("foo", "bar")
+                .verifyComplete();
+    }
 
-//========================================================================================
+    //========================================================================================
+    @Test
+    void fromList() {
+        StepVerifier.create(workshop.fooBarFluxFromList())
+                .expectNext("foo", "bar")
+                .verifyComplete();
+    }
 
-	@Test
-	public void fromValues() {
-		Flux<String> flux = workshop.fooBarFluxFromValues();
-		StepVerifier.create(flux)
-				.expectNext("foo", "bar")
-				.verifyComplete();
-	}
+    //========================================================================================
+    @Test
+    void error() {
+        StepVerifier.create(workshop.errorFlux())
+                .verifyError(IllegalStateException.class);
+    }
 
-//========================================================================================
-
-	@Test
-	public void fromList() {
-		Flux<String> flux = workshop.fooBarFluxFromList();
-		StepVerifier.create(flux)
-				.expectNext("foo", "bar")
-				.verifyComplete();
-	}
-
-//========================================================================================
-
-	@Test
-	public void error() {
-		Flux<String> flux = workshop.errorFlux();
-		StepVerifier.create(flux)
-				.verifyError(IllegalStateException.class);
-	}
-
-//========================================================================================
-
-	@Test
-	public void countEach100ms() {
-		Flux<Long> flux = workshop.counter();
-		StepVerifier.create(flux)
-				.expectNext(0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L)
-				.verifyComplete();
-	}
-
+    //========================================================================================
+    @Test
+    void countEach100ms() {
+        StepVerifier.create(workshop.counter())
+                .expectNext(0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L)
+                .verifyComplete();
+    }
 }
