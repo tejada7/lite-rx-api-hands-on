b988afc9082ec79f3f6633bc582f7a2145040d71
Favio Tejada <favio_tejada@hotmail.com>
2022-02-14 14:54:46 +0100

    Solution part 10 reactive to blocking

--- src/main/java/io/pivotal/literx/Part10ReactiveToBlocking.java
+++ src/main/java/io/pivotal/literx/Part10ReactiveToBlocking.java
@@ -11,18 +11,14 @@ import reactor.core.publisher.Mono;
  */
 public class Part10ReactiveToBlocking {
 
-//========================================================================================
+    //========================================================================================
+    User monoToValue(Mono<User> mono) {
+        return mono.block();
+    }
 
-	// TODO Return the user contained in that Mono
-	User monoToValue(Mono<User> mono) {
-		return null;
-	}
-
-//========================================================================================
-
-	// TODO Return the users contained in that Flux
-	Iterable<User> fluxToValues(Flux<User> flux) {
-		return null;
-	}
+    //========================================================================================
+    Iterable<User> fluxToValues(Flux<User> flux) {
+        return flux.toIterable();
+    }
 
 }
