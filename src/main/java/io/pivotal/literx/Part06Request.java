84cb1cf8289b05fdcbb8eb5c88ae4bb3adf67c63
Favio Tejada <favio_tejada@hotmail.com>
2022-02-14 12:07:42 +0100

    Solution part 6 request

--- src/main/java/io/pivotal/literx/Part06Request.java
+++ src/main/java/io/pivotal/literx/Part06Request.java
@@ -6,6 +6,8 @@ import io.pivotal.literx.repository.ReactiveUserRepository;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import static java.lang.System.out;
+
 /**
  * Learn how to control the demand.
  *
@@ -13,34 +15,35 @@ import reactor.test.StepVerifier;
  */
 public class Part06Request {
 
-	ReactiveRepository<User> repository = new ReactiveUserRepository();
+    ReactiveRepository<User> repository = new ReactiveUserRepository();
 
-//========================================================================================
+    //========================================================================================
+    StepVerifier requestAllExpectFour(Flux<User> flux) {
+        return StepVerifier.create(flux)
+                .expectNextCount(4)
+                .expectComplete();
+    }
 
-	// TODO Create a StepVerifier that initially requests all values and expect 4 values to be received
-	StepVerifier requestAllExpectFour(Flux<User> flux) {
-		return null;
-	}
+    //========================================================================================
+    StepVerifier requestOneExpectSkylerThenRequestOneExpectJesse(Flux<User> flux) {
+        return StepVerifier.create(flux)
+                .expectNext(User.SKYLER)
+                .expectNext(User.JESSE)
+                .thenCancel();
+    }
 
-//========================================================================================
+    //========================================================================================
+    Flux<User> fluxWithLog() {
+        return repository.findAll()
+                .log();
+    }
 
-	// TODO Create a StepVerifier that initially requests 1 value and expects User.SKYLER then requests another value and expects User.JESSE then stops verifying by cancelling the source
-	StepVerifier requestOneExpectSkylerThenRequestOneExpectJesse(Flux<User> flux) {
-		return null;
-	}
-
-//========================================================================================
-
-	// TODO Return a Flux with all users stored in the repository that prints automatically logs for all Reactive Streams signals
-	Flux<User> fluxWithLog() {
-		return null;
-	}
-
-//========================================================================================
-
-	// TODO Return a Flux with all users stored in the repository that prints "Starring:" on subscribe, "firstname lastname" for all values and "The end!" on complete
-	Flux<User> fluxWithDoOnPrintln() {
-		return null;
-	}
+    //========================================================================================
+    Flux<User> fluxWithDoOnPrintln() {
+        return repository.findAll()
+                .doOnSubscribe(subscription -> out.println("Starring:"))
+                .doOnNext(user -> out.println(user.getFirstname() + " " + user.getLastname()))
+                .doOnComplete(() -> out.println("The end!"));
+    }
 
 }
--- src/test/java/io/pivotal/literx/Part06RequestTest.java
+++ src/test/java/io/pivotal/literx/Part06RequestTest.java
@@ -22,102 +22,98 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Sebastien Deleuze
  */
-public class Part06RequestTest {
+class Part06RequestTest {
 
-	Part06Request workshop = new Part06Request();
-	ReactiveRepository<User> repository = new ReactiveUserRepository();
+    Part06Request workshop = new Part06Request();
+    ReactiveRepository<User> repository = new ReactiveUserRepository();
 
-	PrintStream originalConsole = System.out;
+    PrintStream originalConsole = System.out;
 
-	@Nullable //null when not useful
-	ByteArrayOutputStream logConsole;
+    @Nullable //null when not useful
+    ByteArrayOutputStream logConsole;
 
-	@AfterEach
-	public void afterEach() {
-		if (logConsole != null) {
-			originalConsole.println(logConsole.toString());
-			System.setOut(originalConsole);
-			logConsole = null;
-		}
-	}
+    @AfterEach
+    public void afterEach() {
+        if (logConsole != null) {
+            originalConsole.println(logConsole.toString());
+            System.setOut(originalConsole);
+            logConsole = null;
+        }
+    }
 
-//========================================================================================
+    //========================================================================================
+    @Test
+    void requestAll() {
+        StepVerifier verifier = workshop.requestAllExpectFour(repository.findAll());
+        verifier.verify();
+    }
 
-	@Test
-	public void requestAll() {
-		Flux<User> flux = repository.findAll();
-		StepVerifier verifier = workshop.requestAllExpectFour(flux);
-		verifier.verify();
-	}
-
-//========================================================================================
-
-	@Test
-	public void requestOneByOne() {
-		Flux<User> flux = repository.findAll();
-		StepVerifier verifier = workshop.requestOneExpectSkylerThenRequestOneExpectJesse(flux);
-		verifier.verify();
-	}
+    //========================================================================================
+    @Test
+    void requestOneByOne() {
+        StepVerifier verifier = workshop.requestOneExpectSkylerThenRequestOneExpectJesse(repository.findAll());
+        verifier.verify();
+    }
 
 //========================================================================================
 
-	@Test
-	public void experimentWithLog() {
-		logConsole = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(logConsole));
+    @Test
+    void experimentWithLog() {
+        logConsole = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(logConsole));
 
-		Flux<User> flux = workshop.fluxWithLog();
+        Flux<User> flux = workshop.fluxWithLog();
 
-		StepVerifier.create(flux, 0)
-		            .thenRequest(1)
-		            .expectNextMatches(u -> true)
-		            .thenRequest(1)
-		            .expectNextMatches(u -> true)
-		            .thenRequest(2)
-		            .expectNextMatches(u -> true)
-		            .expectNextMatches(u -> true)
-		            .verifyComplete();
+        StepVerifier.create(flux, 0)
+                .thenRequest(1)
+                .expectNextMatches(u -> true)
+                .thenRequest(1)
+                .expectNextMatches(u -> true)
+                .thenRequest(2)
+                .expectNextMatches(u -> true)
+                .expectNextMatches(u -> true)
+                .verifyComplete();
 
-  		List<String> log = Arrays.stream(logConsole.toString().split(System.lineSeparator()))
-							   .filter(s -> s.contains("] INFO"))
-							   .map(s -> s.replaceAll(".*] INFO .* - ", ""))
-							   .collect(Collectors.toList());
+        List<String> log = Arrays.stream(logConsole.toString().split(System.lineSeparator()))
+                .filter(s -> s.contains("] INFO"))
+                .map(s -> s.replaceAll(".*] INFO .* - ", ""))
+                .collect(Collectors.toList());
 
-		assertThat(log)
-				.containsExactly("onSubscribe(FluxZip.ZipCoordinator)"
-						, "request(1)"
-						, "onNext(Person{username='swhite', firstname='Skyler', lastname='White'})"
-						, "request(1)"
-						, "onNext(Person{username='jpinkman', firstname='Jesse', lastname='Pinkman'})"
-						, "request(2)"
-						, "onNext(Person{username='wwhite', firstname='Walter', lastname='White'})"
-						, "onNext(Person{username='sgoodman', firstname='Saul', lastname='Goodman'})"
-						, "onComplete()");
-	}
+        assertThat(log)
+                .containsExactly("onSubscribe(FluxZip.ZipCoordinator)"
+                        , "request(1)"
+                        , "onNext(Person{username='swhite', firstname='Skyler', lastname='White'})"
+                        , "request(1)"
+                        , "onNext(Person{username='jpinkman', firstname='Jesse', lastname='Pinkman'})"
+                        , "request(2)"
+                        , "onNext(Person{username='wwhite', firstname='Walter', lastname='White'})"
+                        , "onNext(Person{username='sgoodman', firstname='Saul', lastname='Goodman'})"
+                        , "onComplete()");
+    }
 
 //========================================================================================
 
-	@Test
-	public void experimentWithDoOn() {
-		Flux<User> flux = workshop.fluxWithDoOnPrintln();
+    @Test
+    void experimentWithDoOn() {
+        Flux<User> flux = workshop.fluxWithDoOnPrintln();
 
-		//setting up the logConsole here should ensure we only capture console logs from the Flux
-		logConsole = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(logConsole));
+        //setting up the logConsole here should ensure we only capture console logs from the Flux
+        logConsole = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(logConsole));
 
-		StepVerifier.create(flux)
-		            .expectNextCount(4)
-		            .verifyComplete();
+        StepVerifier.create(flux)
+                .expectNextCount(4)
+                .verifyComplete();
 
-		String[] log = logConsole.toString().split(System.lineSeparator());
+        String[] log = logConsole.toString().split(System.lineSeparator());
 
-		assertThat(log)
-				.containsExactly("Starring:"
-						, "Skyler White"
-						, "Jesse Pinkman"
-						, "Walter White"
-						, "Saul Goodman"
-						, "The end!");
-	}
+        assertThat(log)
+                .containsExactly("Starring:"
+                        , "Skyler White"
+                        , "Jesse Pinkman"
+                        , "Walter White"
+                        , "Saul Goodman"
+                        , "The end!");
+    }
 
 }
